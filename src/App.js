2ad69d0d3800db3d8c3a5bcbf32e0c3ed8c8bd7a
Béatrice <beatrice.briand@viacesi.fr>
2022-02-24 12:09:33 +0100

    push form + navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { UidContext } from "./components/AppContext";
 import Routes from "./components/Routes";
 
 function App() {
 	const [uid, setUid] = useState(null);
+	const dispatch = useDispatch();
 
 
   return (
--- src/pages/Profil.js
+++ src/pages/Profil.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import Log from "../components/Log";
+import { UidContext } from "../components/AppContext";
+import UpdateProfil from "../components/Profil/UpdateProfil";
 
 const Profil = () => {
+  const uid = useContext(UidContext);
 
-	return (
-		<div>
-			Profil
-		</div>
-	)
-}
+  return (
+    <div className="profil-page">
+      {uid ? (
+        <UpdateProfil/>
+      ) : (
+        <div className="log-container">
+          <Log signin={false} signup={true} />
+          <div className="img-container">
+            <img src="./img/log.svg" alt="img-log" />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Profil;
