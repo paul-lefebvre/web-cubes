fa08e96cd0d78e4262ad687f655f48016243ca22
Béatrice <beatrice.briand@viacesi.fr>
2022-03-18 14:42:55 +0100

    style modifié

--- src/components/Utils.js
+++ src/components/Utils.js
@@ -1,8 +1,5 @@
 export const dateParser = (num) => {
   let options = {
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
     weekday: "long",
     year: "numeric",
     month: "short",
