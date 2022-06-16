ec5390b235f5016d563f3b349237f0e610cc8d98
Béatrice <beatrice.briand@viacesi.fr>
2022-06-16 16:16:49 +0200

    test des elements présents

--- tests__e2e/index.spec.js
+++ tests__e2e/index.spec.js
@@ -12,6 +12,27 @@ test.describe('basic test', async () => {
 		const navbar = await page.$$("nav div.nav-container")
 		expect(navbar).toBeTruthy();
 	  });
+
+	  test('is leftnav loaded', async ({ page }) => {
+		await page.goto("http://localhost:3006")
+	
+		const leftnav = await page.$$("div div.home div.left-nav-container")
+		expect(leftnav).toBeTruthy();
+	  });
+
+	  test('are the trends visible', async ({ page }) => {
+		await page.goto("http://localhost:3006")
+	
+		const trends = await page.$$("div.home div.left-nav-container div.main div.right-side div.wrapper div.trending-container")
+		expect(trends).toBeTruthy();
+	  });
+
+	  test('are the suggestions visible', async ({ page }) => {
+		await page.goto("http://localhost:3006")
+	
+		const trends = await page.$$("div.home div.right-side div.right-side-container div.wrapper div.trending-container div.get-friends-container")
+		expect(trends).toBeTruthy();
+	  });
 	  
   
 });
